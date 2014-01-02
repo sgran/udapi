a758df4fa954e1ae4f0e3e69caf5a343c0abfb59
Stephen Gran <steve@lobefin.net>
2014-01-02 18:08:07 +0000

    Automatically generate list of subkeys
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- udapi/controllers/root.py
+++ udapi/controllers/root.py
@@ -1,14 +1,18 @@
 from pecan import expose
 from webob.exc import status_map
 
-import udapi.controllers.v1
+from udapi.controllers.v1 import V1Controller
 
 
 class RootController(object):
 
+    def __init__(self):
+        super(RootController, self).__init__()
+        self.v1 = V1Controller()
+
     @expose('json')
     def index(self):
-        return {'versions': ['v1']}
+        return {'versions': self.__dict__.keys()}
 
     @expose('error.html')
     def error(self, status):
@@ -18,4 +22,3 @@ class RootController(object):
             status = 500
         message = getattr(status_map.get(status), 'explanation', '')
         return dict(status=status, message=message)
-    v1 = udapi.controllers.v1.V1Controller()
--- udapi/controllers/v1/__init__.py
+++ udapi/controllers/v1/__init__.py
@@ -5,10 +5,14 @@ from udapi.controllers.v1.autocert import AutoCertController
 
 
 class V1Controller(object):
+
+    def __init__(self):
+        super(V1Controller, self).__init__()
+        self.hosts = HostController()
+        self.certs = CertController()
+        self.autoca = AutoCertController()
+
     @expose('json')
     def index(self):
-        return ['autoca', 'certs', 'hosts']
+        return sorted(self.__dict__.keys())
 
-    hosts = HostController()
-    certs = CertController()
-    autoca = AutoCertController()
